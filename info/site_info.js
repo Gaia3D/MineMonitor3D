eed6ef2ccc398ee2647a12b55426f931ba96ef45
BJ Jang <bjjang@gaia3d.com>
2016-10-24 22:48:58 +0900

    자료구조 변경, 카메라 위치 설정

--- info/site_info.js
+++ info/site_info.js
@@ -5,8 +5,6 @@ var siteInfo = {};
 siteInfo["lafarge"] =
 {
 	title: "라파즈 한라시멘트 - 가행광산",
-  photoYearArray:[2007,2008,2010,2013,2014,2015,2016], 
-  demYearArray:[2007,2014,2015,2016],
 	view: {
         destination : Cesium.Cartesian3.fromDegrees(128.96, 37.52, 3000.0),
         orientation : {
@@ -15,24 +13,27 @@ siteInfo["lafarge"] =
             roll : 0.0
         }
     },
-	chg_type: [
-		{text: "생태복원지역", action: function(){setWmsLayer({"theme":"restore"});}}
-		,{text: "채광지역", action: function(){setWmsLayer({"theme":"dig"});}}
-		,{text: "지형변화분석", action: function(){setWmsLayer({"theme":"dem_delta"});}}
-		,{text: "차영상", action: function(){setWmsLayer({"theme":"photo_delta"});}}
-	],
-	period: [
-		{text: "2015~2016", action: function(){setDem("dem_2016"); setWmsLayer({"year":2016,"photo":"photo"});}}
-		,{text: "2014~2015", action: function(){setDem("dem_2015"); setWmsLayer({"year":2015,"photo":"photo"});}}
-	],
-	message: "광산 내의 각 지역을 선택하면 변화에 해당하는 토공량을 볼 수 있습니다."
+  photoYearArray:[
+    {year:2007,layer:"Sangji:2007_WGS84"},
+    {year:2008,layer:"Sangji:2008_WGS84"},
+    {year:2010,layer:"Sangji:2010_WGS84"},
+    {year:2012,layer:"Sangji:2012_WGS84"},
+    {year:2013,layer:"Sangji:2013_WGS84"},
+    {year:2014,layer:"Sangji:2014_WGS84"},
+    {year:2015,layer:"Sangji:2015_UAV_wgs84"},
+    {year:2016,layer:"Sangji:2016_merge_WGS84"}
+  ], 
+  demYearArray:[
+    {year:2007,layer:"dem_2007"},
+    {year:2014,layer:"dem_2014"},
+    {year:2015,layer:"dem_2015"},
+    {year:2016,layer:"dem_2016"}
+  ]
 }
 
 siteInfo["dongyang"] =
 {
 	title: "동양시멘트 - 휴지광산",
-  photoYearArray:[2014,2016], 
-  demYearArray:[2014],
 	view: {
         destination : Cesium.Cartesian3.fromDegrees(129.172, 37.385, 2000.0),
         orientation : {
@@ -41,98 +42,87 @@ siteInfo["dongyang"] =
             roll : 0.0
         }
 	},
-	chg_type: [
-		{text: "종합비교", selected: true, action: function(){console.log("종합비교");}}
-		,{text: "생태복원지역", action: function(){console.log("생태복원지역");}}
-		,{text: "채광지역", action: function(){console.log("채광지역");}}
-		,{text: "재해지역", action: function(){console.log("재해지역");}}
-		,{text: "지형변화분석", action: function(){console.log("지형변화분석");}}
-		,{text: "차영상", action: function(){console.log("차영상");}}
-	],
-	period: [
-		{text: "2014~2015", action: function(){console.log("2014~2015");}}
-		,{text: "2015~2016", action: function(){console.log("2015~2016");}}
-	],
-	message: "광산 내의 각 지역을 선택하면 변화에 해당하는 토공량을 볼 수 있습니다."
+  photoYearArray:[
+    {year:2008,layer:"Sangji:2008_dongyang_wgs84"},
+    {year:2010,layer:"Sangji:2010_dongyang_WGS84"},
+    {year:2012,layer:"Sangji:2012_dongyang_WGS84"}
+  ], 
+  demYearArray:[
+    {year:2007,layer:"dem_2007"},
+    {year:2014,layer:"dem_2014"},
+    {year:2015,layer:"dem_2015"},
+    {year:2016,layer:"dem_2016"}
+  ]
 }
 
-///////
-// UI BOX FUNCTION
-function runCmd(cmd, option) {
-	if (cmd == undefined)
-		return;
-	
-	try {
-		console.log(cmd);
-		var fun = eval(cmd);
-		if (option == undefined) 
-			fun();
-		else
-			fun(option);
-	} catch(err) {
-		console.error("[runCmd]"+err.message);
-	}
+siteInfo["hyundai"] =
+{
+	title: "현대시멘트 - 가행광산",
+	view: {
+        destination : Cesium.Cartesian3.fromDegrees(128.360, 37.251, 1600.0),
+        orientation : {
+            heading : Cesium.Math.toRadians(356.2804),
+            pitch : Cesium.Math.toRadians(-27.3142),
+            roll : 0
+        }
+	},
+  photoYearArray:[
+    {year:2007,layer:"Sangji:Hyundai_2007_WGS84"},
+    {year:2009,layer:"Sangji:Hyundai_2009_WGS84"},
+    {year:2012,layer:"Sangji:Hyundai_2012_WGS84"}
+  ], 
+  demYearArray:[
+    {year:2007,layer:"dem_2007"},
+    {year:2014,layer:"dem_2014"},
+    {year:2015,layer:"dem_2015"},
+    {year:2016,layer:"dem_2016"}
+  ]
 }
 
-var actionDic = {};
-function showInfo(infoObj) {
-	var htmlList = [];
-	
-	htmlList.push('<h1>');
-	htmlList.push(infoObj.title);
-	htmlList.push('</h1>');
-	htmlList.push('<label for="chg_type">• 변화종류&nbsp;</label>');
-	htmlList.push('<select name="chg_type" id="chg_type">');
-	for (var i in infoObj.chg_type) {
-		var item = infoObj.chg_type[i]; 
-		var id = "chg_type_"+i;
-		actionDic[id] = item.action;
-		if (item.selected)
-			htmlList.push('<option value="'+id+'" selected="selected">');
-		else
-			htmlList.push('<option value="'+id+'">');
-		htmlList.push(item.text);
-		htmlList.push('</option>');
-	}
-	htmlList.push('</select>');
-	htmlList.push('<br/>');
-	htmlList.push('<label for="period">• 비교시기&nbsp;</label>');
-	htmlList.push('<select name="period" id="period">');
-	for (var i in infoObj.period) {
-		var item = infoObj.period[i]; 
-		var id = "period_"+i;
-		actionDic[id] = item.action;
-		if (item.selected)
-			htmlList.push('<option value="'+id+'" selected="selected">');
-		else
-			htmlList.push('<option value="'+id+'">');
-		htmlList.push(item.text);
-		htmlList.push('</option>');
-	}
-	htmlList.push('</select>');
-	htmlList.push('<br/>');
-	htmlList.push('<span>');
-	htmlList.push(infoObj.message);
-	htmlList.push('</span>');
-	
-	var ui_box = $(".ui-box");
-	ui_box.hide();
-	ui_box.html(htmlList.join(""));
-
-	$( ".ui-box select" )
-	  .change(function () {
-		var id = $( this ).val();
-		actionDic[id]();
-	//});
-	}).change();
-	ui_box.show("drop", {}, 500);
-	
-	if (infoObj.view)
-		viewer.camera.flyTo(infoObj.view);
+siteInfo["asia"] =
+{
+	title: "아시아시멘트 - 가행광산",
+	view: {
+        destination : Cesium.Cartesian3.fromDegrees(128.301, 37.251, 1550),
+        orientation : {
+            heading : Cesium.Math.toRadians(21.6944),
+            pitch : Cesium.Math.toRadians(-39.6105),
+            roll : 0
+        }
+	},
+  photoYearArray:[
+    {year:2007,layer:"Sangji:Asia_2007_WGS84"},
+    {year:2009,layer:"Sangji:Asia_2009_WGS84"},
+    {year:2012,layer:"Sangji:Asia_2012_WGS84"}
+  ], 
+  demYearArray:[
+    {year:2007,layer:"dem_2007"},
+    {year:2014,layer:"dem_2014"},
+    {year:2015,layer:"dem_2015"},
+    {year:2016,layer:"dem_2016"}
+  ]
 }
 
-function hideInfo() {
-	$(".ui-box").hide();
+siteInfo["ssangyoung"] =
+{
+	title: "쌍용시멘트 - 휴지광산",
+	view: {
+        destination : Cesium.Cartesian3.fromDegrees(129.170, 37.391, 1020.0),
+        orientation : {
+            heading : Cesium.Math.toRadians(344.583),
+            pitch : Cesium.Math.toRadians(-37.049),
+            roll : 0
+        }
+	},
+  photoYearArray:[
+    {year:2008,layer:"Sangji:SSangyoung_2008_WGS84"},
+    {year:2009,layer:"Sangji:SSangyoung_2009_WGS84"},
+    {year:2011,layer:"Sangji:SSangyoung_2011_WGS84"}
+  ], 
+  demYearArray:[
+    {year:2007,layer:"dem_2007"},
+    {year:2014,layer:"dem_2014"},
+    {year:2015,layer:"dem_2015"},
+    {year:2016,layer:"dem_2016"}
+  ]
 }
-
-
